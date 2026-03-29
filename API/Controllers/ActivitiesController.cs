44e2ce131d8b982bcae7faf9e7c506e37c963bf6
rotemustacchi <156905035+rotemustacchi@users.noreply.github.com>
2026-03-29 12:28:31 +0300

    End of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController(AppDbContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetails(string id)
+        {
+            var activity = await context.Activities.FindAsync(id);
+            if(activity == null) return NotFound();
+            return activity;
+        }
+    }
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options): DbContext(options)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
